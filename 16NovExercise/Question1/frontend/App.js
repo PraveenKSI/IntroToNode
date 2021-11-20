382df41eb3b7e9b4df6e796939914c7257ce7107
Praveen Kumar Singh <49147942+PraveenKSI@users.noreply.github.com>
2021-11-20 21:37:07 +0530

    Add files via upload

---
+++ 16NovExercise/Question1/frontend/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import QueryForm from './component/QueryForm';
+function App() {
+  return (
+    <div className="App">
+      <QueryForm/>
+    </div>
+  );
+}
+
+export default App;
